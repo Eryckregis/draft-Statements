a1228a5d29944d77c27c90e07c175aa6cde662ec
eryck regis da silva <eryckengsoftware@gmail.com>
2026-02-01 11:53:50 -0300

    projeto criado

---
+++ src/main/java/com/statementapi/draft/statements/DraftStatementsApplication.java
@@ -0,0 +1,13 @@
+package com.statementapi.draft.statements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DraftStatementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DraftStatementsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/statementapi/draft/statements/DraftStatementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.statementapi.draft.statements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DraftStatementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
